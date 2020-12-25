86f1ab5cfcfd47365fc2edcc7f1bc0d8032af4b1
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-12-25 22:50:44 +0000

    add redux, react_redux, store, redux devtools extension

--- src/index.js
+++ src/index.js
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore, applyMiddleware,compose} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import {combineReducers} from 'redux' 
+
+const rootReducer = combineReducers({
+})
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+      <App />
+  </Provider>,
+      document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
